795f9df0ed23378438ad4d8a110cc3e0a30b83f5
Vanderson <vandersontavares32@gmail.com>
2023-01-24 19:10:22 -0300

    handleClick, react-icons

--- src/pages/Store/index.jsx
+++ src/pages/Store/index.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { BsFillCartCheckFill, BsFillCartPlusFill } from "react-icons/bs";
 
 const Store = () => {
   const [data, setData] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     axios
@@ -16,13 +18,34 @@ const Store = () => {
       });
   }, []);
 
+  function handleClick(obj){
+    const element = cart.find((e)=> e.id === obj.id )
+    if(element){
+        const arrFilter = cart.filter((e)=> e.id !== obj.id)
+        setCart(arrFilter);
+    }else{
+        setCart([...cart, obj])
+    }
+  }
+
   return (
     <div>
       <h1>Store</h1>
       <div>
         {data.map((e) => {
           return (
-            <div key={e.id}>{e.title} <img src={e.thumbnail} alt="celphone-pic"/></div>
+            <div key={e.id}>
+              <h4>{e.title}</h4>
+              <img src={e.thumbnail} alt="celphone-pic" />
+              <h4>R${e.price}</h4>
+              <button onClick={()=> handleClick(e)}>
+                {cart.some((itemCart) => itemCart.id === e.id) ? (
+                  <BsFillCartCheckFill />
+                ) : (
+                  <BsFillCartPlusFill />
+                )}
+              </button>
+            </div>
           );
         })}
       </div>
